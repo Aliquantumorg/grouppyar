bb8ed65838287fd742336e639f93aa6884374b7d
ALIQUANTUMORGG <tahaaghaeiaghaei@gmail.com>
2026-07-23 16:08:08 -0700

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -8,4 +8,12 @@ def home():
     return "GroupYar is running!"
 
 def start_webserver():
-    app.run(host="0.0.0.0", port=10000)
+    thread = threading.Thread(
+        target=lambda: app.run(
+            host="0.0.0.0",
+            port=10000,
+            use_reloader=False
+        ),
+        daemon=True
+    )
+    thread.start()
